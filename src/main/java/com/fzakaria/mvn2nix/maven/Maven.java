f9b93f6773111eb1b75c2807ef5361fb381c62fa
Jannik Vierling <jannik.vierling@gmail.com>
2023-11-25 20:17:08 +0100

    remove unused import

--- src/main/java/com/fzakaria/mvn2nix/maven/Maven.java
+++ src/main/java/com/fzakaria/mvn2nix/maven/Maven.java
@@ -16,7 +16,6 @@ import org.apache.maven.shared.invoker.PrintStreamHandler;
 import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
